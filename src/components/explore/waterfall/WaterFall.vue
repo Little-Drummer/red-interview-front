c9db17d545929be0bc73a046a72a59af8cdfafda
YJXW <1085561450@qq.com>
2024-02-15 13:05:12 +0800

    修复更改文件名的bug

--- src/components/explore/waterfall/WaterFall.vue
+++ src/components/explore/waterfall/WaterFall.vue
@@ -2,7 +2,7 @@
 import { computed, ref } from 'vue'
 import type { PostPage } from '@/type/noteType.ts'
 import FeedsFooter from '@/components/explore/waterfall/footer/FeedsFooter.vue'
-import Cover from '@/components/explore/waterfall/Cover.vue'
+import CoverImage from '@/components/explore/waterfall/CoverImage.vue'
 const props = withDefaults(
   defineProps<{
     noteListPage: PostPage
@@ -45,18 +45,7 @@ const updateLayout = (item: HTMLElement) => {
     v-memo="item.postId"
   >
     <div>
-      <!--      <router-link-->
-      <!--        to="to"-->
-      <!--        :style="{-->
-      <!--          height: '334px',-->
-      <!--          background:-->
-      <!--            'url(' +-->
-      <!--            item.images[0].imageUrl +-->
-      <!--            ') center center / cover no-repeat'-->
-      <!--        }"-->
-      <!--        class="cover mask ld"-->
-      <!--      ></router-link>-->
-      <cover :image-url="item.images[0].imageUrl" />
+      <cover-image :image-url="item.images[0].imageUrl" />
       <feeds-footer
         :title="item.title"
         :name="item.author.name"
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ExploreView from '@/views/ExploreView.vue'
-import { useMiscStore } from '@/stores/modules/misc.ts'
+// import { useMiscStore } from '@/stores/modules/misc.ts'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,31 +20,28 @@ const router = createRouter({
       name: 'notification',
       component: () => import('../views/NotificationView.vue')
     }
-  ],
-  scrollBehavior(to, from, savedPosition) {
-    const MiscStore = useMiscStore()
-    // console.log(to)
-    // if (savedPosition) {
-    //   return savedPosition
-    // }
-
-    if (to.path === '/explore') {
-      return { top: 1000, behavior: 'smooth', el: '.feeds-page' }
-      // return new Promise((resolve, reject) => {
-      //   setTimeout(() => {
-      //     console.log(MiscStore.feedsScrollPosition, '---------')
-      //     resolve({
-      //       top: 1000,
-      //       behavior: 'smooth',
-      //       el: '.el-main'
-      //     })
-      //   }, 1000)
-      // })
-
-      // return { top: MiscStore.feedsScrollPosition, behavior: 'smooth' }
-    } else return { top: 1000, behavior: 'smooth' }
-  }
+  ]
+  // scrollBehavior(to, from, savedPosition) {
+  //   const MiscStore = useMiscStore()
+  // console.log(to)
+  // if (savedPosition) {
+  //   return savedPosition
   // }
+  // if (to.path === '/explore') {
+  // return { top: 1000, behavior: 'smooth', el: '.feeds-page' }
+  // return new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     console.log(MiscStore.feedsScrollPosition, '---------')
+  //     resolve({
+  //       top: 1000,
+  //       behavior: 'smooth',
+  //       el: '.el-main'
+  //     })
+  //   }, 1000)
+  // })
+  // return { top: MiscStore.feedsScrollPosition, behavior: 'smooth' }
+  //     } else return { top: 1000, behavior: 'smooth' }
+  //   }
 })
 
 export default router
